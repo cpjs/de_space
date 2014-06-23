7383d7eff8465a527183024f11c11fb6a585f48d
glurb <cpjs2@kent.ac.uk>
2014-06-23 18:56:58 +0100

    second commit

--- app.go
+++ app.go
@@ -6,10 +6,7 @@ import "bufio"
 import "regexp"
 import "math"
 
-type crystal struct {
-	a, b, c, α, β, γ 	float64
-	symmetry 			string
-}
+type crystal struct { a, b, c, α, β, γ float64 }
 
 var cs = map[int]string {
     1: "Triclinic",
@@ -20,8 +17,23 @@ var cs = map[int]string {
     6: "Hexagonal",
     7: "Cubic",
 }
-
 var prompt string = "> "
+var pi float64 = math.Pi
+
+func rad(in float64) (float64) { return (in * pi/180.0) }
+func deg(in float64) (float64) { return (in * 180.0/pi) }
+
+// takes deg. as in/out, computes in rads:
+
+func sin(in float64) (float64)  { return deg(math.Sin(rad(in))) }
+func cos(in float64) (float64)  { return deg(math.Cos(rad(in))) }
+func tan(in float64) (float64)  { return deg(math.Tan(rad(in))) }
+func isin(in float64) (float64) { return deg(math.Asin(rad(in))) }
+func icos(in float64) (float64) { return deg(math.Acos(rad(in))) }
+func itan(in float64) (float64) { return deg(math.Atan(rad(in))) }
+func pow(b,e float64) (float64) { return math.Pow(b,e) }
+
+// get stdin input, error check
 
 func getLatticeParam(q string) (f float64) {
 	stdin := bufio.NewReader(os.Stdin)
@@ -33,6 +45,8 @@ func getLatticeParam(q string) (f float64) {
 		fmt.Print(q+prompt)
 	}
 }
+
+// get stdin input, error check + regular expressions
 
 func getCrystalSys() (i int) {
 
@@ -60,10 +74,12 @@ func getCrystalSys() (i int) {
     }
 }
 
+// program starts here!
+
 func main() {
 
     var c crystal
-    var h, k, l, ds float64
+    var h, k, l, ds, th float64
 
     fmt.Println("------ Please selet your crystal/lattice system ----")
     for i := 1; i < 8; i++ {
@@ -73,7 +89,7 @@ func main() {
     fmt.Println("")
 
     fmt.Println("------ Please enter wavelength of x-rays -----------")
-    Wavelength := getLatticeParam("λ (nm)\n")
+    Wavelength := getLatticeParam("λ (Å)\n")
     fmt.Println("")
 
     fmt.Println("------ Please enter your lattice parameters --------")
@@ -139,61 +155,54 @@ func main() {
     fmt.Printf("   α: %g deg.\n",c.α)
     fmt.Printf("   β: %g deg.\n",c.β)
     fmt.Printf("   γ: %g deg.\n",c.γ)
-    fmt.Printf("   λ: %g nm\n",Wavelength)
+    fmt.Printf("   λ: %g Å\n",Wavelength)
     fmt.Println("")
     fmt.Println("------ h, k, l, d-spacing, 2theta ------------------")
+
+    // d-spacing formulas from Dr. Falak Sher
+    // National Workshop on Crystal Structure Determination using Powder XRD,
+    // organized by the Khwarzimic Science Society, 15 – 17 August 2007 (http://www.khwarzimic.org).
 
     for l = 0.0; l < 3.0; l=l+1.0 {
         for k = 0.0; k < 3.0; k=k+1.0 {
             for h = 0.0; h < 3.0; h=h+1.0 {
                 switch CrystalSys {
                     case 1: //tri
-                        var v1 float64 = math.Pow(math.Cos(c.α),2)
-                        var v2 float64 = math.Pow(math.Cos(c.β),2)
-                        var v3 float64 = math.Pow(math.Cos(c.γ),2)
-                        var f4 float64 = 2*math.Cos(c.α)*math.Cos(c.β)*math.Cos(c.γ)
-                        var v5 float64 = math.Sqrt(1 - v1 - v2 - v3 + v4)
-                        var t1 float64 = 1/math.Pow((c.a*c.b*c.c*v5),2)
-                        var t2 float64 = math.Pow(c.b,2)*math.Pow(c.c,2)*math.Pow(math.Sin(c.α),2)
-                        var t3 float64 = math.Pow(c.a,2)*math.Pow(c.c,2)*math.Pow(math.Sin(c.β),2)
-                        var t4 float64 = math.Pow(c.a,2)*math.Pow(c.b,2)*math.Pow(math.Sin(c.γ),2)
-                        var t5 float64 = math.Pow(c.c,2)*c.a*c.b*(math.Cos(c.α)*math.Cos(c.β)-math.Cos(c.γ))
-                        var t6 float64 = math.Pow(c.a,2)*c.c*c.b*(math.Cos(c.β)*math.Cos(c.γ)-math.Cos(c.α))
-                        var t7 float64 = math.Pow(c.b,2)*c.a*c.c*(math.Cos(c.γ)*math.Cos(c.α)-math.Cos(c.β))
-                        ds = t1*(t2*math.Pow(h,2) + t3*math.Pow(k,2) + t4*math.Pow(l,2) + 2*t5*h*k + 2*t6*k*l + 2*t7*h*l)
-                    case 2: // MONOCLINIC
-                        var t1 float64 = 1/(math.Pow(math.Sin(c.β),2))
-                        var t2 float64 = math.Pow(h,2)/math.Pow(c.a,2)
-                        var t3 float64 = (math.Pow(k,2)*math.Pow(math.Sin(c.β),2))/(math.Pow(c.b,2))
-                        var t4 float64 = math.Pow(l,2)/math.Pow(c.c,2)
-                        var t5 float64 = (2*h*l*math.Cos(c.β))/(c.a*c.c)
-                        ds = t1*(t2 + t3 + t4 - t5)
-                    case 3: // ORTHORHOMBIC
-                        var t1 float64 = math.Pow(h,2)/math.Pow(c.a,2)
-                        var t2 float64 = math.Pow(k,2)/math.Pow(c.b,2)
-                        var t3 float64 = math.Pow(l,2)/math.Pow(c.c,2)
-                        ds = t1 + t2 + t3
-                    case 4: //TETRAGONAL
-                        var t1 float64 = (math.Pow(h,2)+math.Pow(k,2))/math.Pow(c.a,2)
-                        var t2 float64 = math.Pow(l,2)/math.Pow(c.c,2)
-                        ds = t1 + t2
-                    case 5: //TRIGONAL
-                        var t1 float64 = (math.Pow(h,2)+math.Pow(k,2)+math.Pow(l,2))*math.Pow(math.Sin(c.α),2)
-                        var t2 float64 = 2.0*((h*k)+(k*l)+(h*l))*math.Pow(math.Cos(c.α),2)
-                        var t3 float64 = math.Cos(c.α)
-                        var t4 float64 = math.Pow(c.a,2)*(1-(3*math.Pow(math.Cos(c.α),2))+(2*math.Pow(math.Cos(c.α),3)))
-                        ds = (t1 + t2 - t3)/t4
-                    case 6: //HEXAGONAL
-                        var t1 float64 = (4.0/3.0)*((math.Pow(h,2)+h*k+math.Pow(k,2))/math.Pow(c.a,2))
-                        var t2 float64 = math.Pow(l,2)/math.Pow(c.c,2)
-                        ds = t1 + t2
-                    case 7: //cubic
-                        ds = (math.Pow(h,2) + math.Pow(k,2) + math.Pow(l,2))/math.Pow(c.a,2)
+                        var vl float64 = math.Sqrt(1 - (pow(cos(c.α),2)) - (pow(cos(c.β),2)) - (pow(cos(c.γ),2)) + (2.0*cos(c.α)*cos(c.β)*cos(c.γ)))
+                        var t1 float64 = 1/pow((c.a * c.b * c.c * vl),2)
+                        var t2 float64 = pow(c.b,2) * pow(c.c,2) * pow(sin(c.α),2)
+                        var t3 float64 = pow(c.a,2) * pow(c.c,2) * pow(sin(c.β),2)
+                        var t4 float64 = pow(c.a,2) * pow(c.b,2) * pow(sin(c.γ),2)
+                        var t5 float64 = pow(c.c,2) * c.a * c.b * (cos(c.α) * cos(c.β) - cos(c.γ))
+                        var t6 float64 = pow(c.a,2) * c.c * c.b * (cos(c.β) * cos(c.γ) - cos(c.α))
+                        var t7 float64 = pow(c.b,2) * c.a * c.c * (cos(c.γ) * cos(c.α) - cos(c.β))
+                        ds = t1*(t2 * pow(h,2) + t3 * pow(k,2) + t4 * pow(l,2) + 2*t5*h*k + 2*t6*k*l + 2*t7*h*l)
+                    case 2: //MONOCLINIC!
+                        var t1 float64 = 1.0/pow(sin(c.β),2)
+                        var t2 float64 = pow(h,2)/pow(c.a,2)
+                        var t3 float64 = pow(k,2) * pow(sin(c.β),2) / pow(c.b,2)
+                        var t4 float64 = pow(l,2)/pow(c.c,2)
+                        var t5 float64 = (2.0 * h * l * cos(c.β)) / (c.a * c.c)
+                        ds = t1 * (t2 + t3 + t4 - t5)
+                    case 3: //ORTHORHOMBIC!
+                        ds = (pow(h,2)/pow(c.a,2)) + (pow(k,2)/pow(c.b,2)) + (pow(l,2)/pow(c.c,2))
+                    case 4: //TETRAGONAL!
+                        ds = (pow(h,2) + pow(k,2))/pow(c.a,2) + pow(l,2)/pow(c.c,2)
+                    case 5: //TRIGONAL!
+                        var t1 float64 = (pow(h,2) + pow(k,2) + pow(l,2)) * pow(sin(c.α),2)
+                        var t2 float64 = 2.0 * (h*k + k*l + h*l) * pow(cos(c.α),2)
+                        var t3 float64 = cos(c.α)
+                        var t4 float64 = pow(c.a,2) * (1.0 - 3.0 * pow(cos(c.α),2) + 2.0 * pow(cos(c.α),3))
+                        ds = (t1 + t2 - t3) / t4
+                    case 6: //HEXAGONAL!
+                        ds = 4.0/3.0 * ((pow(h,2) + h*k + pow(k,2))/pow(c.a,2)) + pow(l,2)/pow(c.c,2)
+                    case 7: //CUBIC!
+                        ds = ( pow(h,2) + pow(k,2) + pow(l,2) ) / pow(c.a,2)
                     default: return
                 }
-                ds = math.Sqrt(math.Pow(ds,-1))
-                th2 := math.Pow(math.Sin(Wavelength/(0.2*ds)),-1)
-                fmt.Printf("%.1g %.1g %.1g   %1.4f    %1.4f\n",h,k,l,ds,th2)
+                ds = 1.0/math.Sqrt(ds)
+                th = deg(isin(Wavelength/(2.0 * ds)))
+                fmt.Printf("%.1g %.1g %.1g   %1.4f    %1.4f\n",h,k,l,ds,th*2.0)
             }
         }
     }
